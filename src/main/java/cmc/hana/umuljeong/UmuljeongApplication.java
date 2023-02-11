e121684d722f21ced7d7d9ee6c399a7c0e5ad4d8
swa07016 <swa07016@naver.com>
2023-02-11 14:25:20 +0900

    feat: project setting

---
+++ src/main/java/cmc/hana/umuljeong/UmuljeongApplication.java
@@ -0,0 +1,13 @@
+package cmc.hana.umuljeong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmuljeongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmuljeongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cmc/hana/umuljeong/UmuljeongApplicationTests.java
@@ -0,0 +1,13 @@
+package cmc.hana.umuljeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmuljeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
