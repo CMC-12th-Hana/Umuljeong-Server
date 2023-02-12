efa25ccd71a27b34ff4a111c2539eafbe6813fe2
swa07016 <swa07016@naver.com>
2023-02-12 20:58:15 +0900

    feat: Add lombok annotations for builder pattern

--- src/main/java/cmc/hana/umuljeong/domain/Business.java
+++ src/main/java/cmc/hana/umuljeong/domain/Business.java
@@ -1,8 +1,14 @@
 package cmc.hana.umuljeong.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Business {
 
     @Id
--- src/main/java/cmc/hana/umuljeong/domain/Company.java
+++ src/main/java/cmc/hana/umuljeong/domain/Company.java
@@ -1,11 +1,17 @@
 package cmc.hana.umuljeong.domain;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Company {
 
     @Id
