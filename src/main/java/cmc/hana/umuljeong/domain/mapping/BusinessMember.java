6d1fecd32272f514fc91322edf5a0013d30659ca
swa07016 <swa07016@naver.com>
2023-02-12 22:36:26 +0900

    feat: Add Business & Member mapping table

---
+++ src/main/java/cmc/hana/umuljeong/domain/mapping/BusinessMember.java
@@ -0,0 +1,27 @@
+package cmc.hana.umuljeong.domain.mapping;
+
+import cmc.hana.umuljeong.domain.Business;
+import cmc.hana.umuljeong.domain.Member;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BusinessMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "business_id")
+    private Business business;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+}
