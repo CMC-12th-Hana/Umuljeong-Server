edd1e6a9032637a6ce05014b4ad6364948f6b1a9
swa07016 <swa07016@naver.com>
2023-02-12 20:16:39 +0900

    feat: add domain layer

---
+++ src/main/java/cmc/hana/umuljeong/domain/Company.java
@@ -0,0 +1,14 @@
+package cmc.hana.umuljeong.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
