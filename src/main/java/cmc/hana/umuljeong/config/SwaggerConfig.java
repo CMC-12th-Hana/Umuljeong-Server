aaac2e137fc7c0059228cb8e0cf6866f75fb516c
swa07016 <swa07016@naver.com>
2023-03-18 00:40:08 +0900

    docs: Add ServersItem in SwaggerConfig

--- src/main/java/cmc/hana/umuljeong/config/SwaggerConfig.java
+++ src/main/java/cmc/hana/umuljeong/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,6 +40,7 @@ public class SwaggerConfig {
                         .bearerFormat("JWT"));
 
         return new OpenAPI()
+                .addServersItem(new Server().url("/"))
                 .info(info)
                 .addSecurityItem(securityRequirement)
                 .components(components);
